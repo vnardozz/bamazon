51c7397b46a708228007c3d18c2e78066bf9316f
vincent <nardozzi24@gmail.com>
2018-06-09 10:40:19 -0400

    added seeds

---
+++ seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)  VALUES ("apples", "food", 4.00, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)  VALUES ("pears", "food", 3.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)  VALUES ("milk", "food", 6.00, 28);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)  VALUES ("candy", "food", 1.00, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)  VALUES ("bread", "food", 3.00, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)  VALUES ("chicken", "food", 7.00, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)  VALUES ("water", "food", .50, 2000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)  VALUES ("tomatos", "food", 2.75, 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)  VALUES ("shrimp", "food", 17.00, 11);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)  VALUES ("steak", "food", 20.99, 40);
